d8bd0bd54738f4af840932bdd94e43c7cec58b57
rohan-mac <rverma98275@gmial.com>
2026-03-11 00:01:45 +0530

    change transpoter

--- backend/src/config/mailer.js
+++ backend/src/config/mailer.js
@@ -1,10 +1,20 @@
 import nodemailer from "nodemailer";
-export const transporter = nodemailer.createTransport({
-  service: "gmail",
+// export const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
+console.log(process.env.EMAIL_PASS);
+console.log(process.env.EMAIL_USER);
+
+ export const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
-  },
+  }
 });
-console.log(process.env.EMAIL_PASS);
-console.log(process.env.EMAIL_USER);
